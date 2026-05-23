dc98c0de4e5e0886a94e1320fe6802aa062b80a7
MalikJunaid553 <malikjunaidawan087@gmail.com>
2026-05-23 15:30:45 +0500

    CRUD Operations With MongoDB

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose"
+import Product from "./model/Product.js"; 
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+async function connectDB(){
+    try{
+        await mongoose.connect("mongodb+srv://junaid:junaidjunaid@cluster0.jj6aivp.mongodb.net/");
+        console.log("Databse connected!")
+    }
+    catch (error){
+        console.log("MongoDB Connecetion Error:" , error )
+    }
+}
+
+connectDB();
+
+app.get("/products" , async( req , res)=>{
+    try{
+        const products = await Product.find()
+        res.json(products)
+    }catch(error){
+        res.status(500).json({message : "Error fetching Products"})
+    }
+})
+
+app.post("/products" , async(req,res)=>{
+    try{
+        const newProductFields = req.body;
+        const newProduct = new Product(newProductFields)
+        await newProduct.save();
+        res.status(201).json(newProduct) 
+    }
+    catch(error){
+        res.status(500).json({message: "Error Creating Product"})
+    }
+})
+
+app.put("/products/:id", async (req, res)=>{
+    try{
+        const {id} = req.params;
+        const updatedProductField = req.body;
+        const updatedProduct = await Product.findOneAndUpdate({id} , updatedProductField , {new: true},);
+        res.json(updatedProduct)
+    }catch(error){
+        res.status(500).json({message: "Error updating product"})
+    }
+})
+app.delete("/products/:id" , async(req,res)=>{
+    try{
+        const {id} = req.params
+        await Product.findOneAndDelete({id});
+        res.status(204).send();
+    }catch(error){
+        res.status(404).json({message: "item not found"})
+    }
+})
+app.listen(3000, ()=>{
+    console.log("Server is running at Port 3000")
+})
